c5fe588627f403b0b45ffd734fbaadb75189f6f3
1bodnar <labodnar14022020@outlook.com>
2021-02-23 23:35:38 +0100

    update the code

--- google/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network_name" {
-  value       = google_compute_network.main.name
-  description = "the name of the new network"
-}
-
-output "private_subnet_self_links" {
-  value       = google_compute_subnetwork.private[*].self_link
-  description = "the self links of the private subnets of the vpc"
-}
